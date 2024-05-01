a687e0809c1cbcc74f8aca7bd74695e48d436926
betajohn <betajohn40@gmail.com>
2024-05-01 08:55:14 -0400

    Add toolbar for editing form pictures field

--- src/components/admin/products/productForm/FormPCarousel.tsx
+++ src/components/admin/products/productForm/FormPCarousel.tsx
@@ -5,8 +5,16 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { Label } from '@/components/ui/label';
 import { useState, useEffect, useCallback } from 'react';
-import { EmblaOptionsType } from 'embla-carousel';
-import Thumb from '@/components/admin/products/productForm/Thumb';
+import { cn } from '@/lib/utils';
+import AddImageSVG from '@/../public/images/addImage.svg';
+import {
+  Plus,
+  Trash2Icon,
+  CornerUpLeft,
+  CornerUpRight,
+  ImageUp,
+} from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
 export default function FormPCarousel({
   form,
@@ -14,8 +22,7 @@ export default function FormPCarousel({
   form: UseFormReturn<ProductFormSchemaType>;
 }) {
   //const { ref, onBlur, onChange } = form.register('images');
-  const watchedImg = form.watch('images');
-  const slides = watchedImg;
+  const slides = form.watch('images');
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel();
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
@@ -44,22 +51,79 @@ export default function FormPCarousel({
     emblaMainApi.on('reInit', onSelect);
   }, [emblaMainApi, onSelect]);
 
+  function addImage() {
+    const newImages = [
+      ...slides,
+      'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+    ];
+
+    form.setValue('images', newImages, {
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  }
+
+  function moveLeft(able: boolean) {
+    if (!able) return;
+    console.log('moving');
+  }
+  function moveRight(able: boolean) {
+    if (!able) return;
+    console.log('moving');
+  }
+  function trash() {
+    console.log('trash');
+  }
+  function uploadHere() {}
+
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3">
       <div>
         <Label>Product Images</Label>
         <div
-          className="embla overflow-hidden w-full rounded-lg aspect-video bg-white"
+          className="embla overflow-hidden w-full rounded-lg aspect-video border-foreground border bg-white"
           ref={emblaMainRef}
         >
-          <div className="embla_container flex">
-            {watchedImg.map((x, i) => {
-              console.log('painting img ' + i);
+          <div className="embla_container flex max-h-full">
+            {[...slides, 'add'].map((x, i) => {
               return (
-                <div key={x} className="flex-[0_0_100%] min-w-0">
-                  <div className="h-full aspect-square relative">
-                    <Image src={x} key={x} alt="Product image" fill />
+                <div
+                  key={x}
+                  className="flex-[0_0_100%] min-w-0 p-2 flex justify-center"
+                >
+                  <div className="grow">
+                    <div className="max-h-full max-w-full aspect-square relative m-auto grow">
+                      {x === 'add' ? (
+                        <AddImageSVG onClick={addImage} />
+                      ) : (
+                        <Image src={x} key={x} alt="Product image" fill />
+                      )}
+                    </div>
                   </div>
+                  {x !== 'add' && (
+                    <div className="flex flex-col items-center m-auto text-gray-900 border border-border p-1 rounded-lg gap-1 h-fit">
+                      <CornerUpLeft
+                        className={cn('w-7 h-7', i === 0 && 'text-gray-300')}
+                        onClick={() => {
+                          moveLeft(i !== 0);
+                        }}
+                      />
+                      <Separator orientation="horizontal" />
+                      <CornerUpRight
+                        className={cn(
+                          'w-7 h-7',
+                          i === slides.length - 1 && 'text-gray-300'
+                        )}
+                        onClick={() => {
+                          moveRight(i !== slides.length - 1);
+                        }}
+                      />
+                      <Separator orientation="horizontal" className="" />
+                      <ImageUp className="w-7 h-7" onClick={uploadHere} />
+                      <Separator orientation="horizontal" />
+                      <Trash2Icon className="w-7 h-7" onClick={trash} />
+                    </div>
+                  )}
                 </div>
               );
             })}
@@ -72,54 +136,27 @@ export default function FormPCarousel({
         ref={emblaThumbsRef}
       >
         <div className="embla-thumbs__container flex">
-          {slides.map((x, index) => (
-            <Thumb
+          {[...slides, 'add'].map((x, index) => (
+            <div
               key={index}
-              onClick={() => onThumbClick(index)}
-              selected={index === selectedIndex}
-              index={index}
-            />
+              className={cn(
+                'embla-thumbs__slide rounded-lg border bg-card text-card-foreground shadow-sm flex-[0_0_22%] min-w-0 ml-3 sm:flex-[0_0_15%] border-foreground',
+                index === selectedIndex && 'border-primary text-primary'
+              )}
+            >
+              <button
+                onClick={() => {
+                  onThumbClick(index);
+                }}
+                type="button"
+                className="w-full h-full"
+              >
+                {x === 'add' ? <Plus className="m-auto h-4" /> : index + 1}
+              </button>
+            </div>
           ))}
         </div>
       </div>
-
-      <Button
-        type="button"
-        onClick={() => {
-          const newImages = [
-            ...watchedImg,
-            'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
-          ];
-
-          form.setValue('images', newImages, {
-            shouldDirty: true,
-            shouldTouch: true,
-          });
-        }}
-      >
-        Add Image
-      </Button>
     </div>
   );
 }
-
-/*
-
-      <Button
-        type="button"
-        onClick={() => {
-          const newImages = [
-            ...watchedImg,
-            'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
-          ];
-
-          form.setValue('images', newImages, {
-            shouldDirty: true,
-            shouldTouch: true,
-          });
-        }}
-      >
-        Add Image
-      </Button>
-
-      */
