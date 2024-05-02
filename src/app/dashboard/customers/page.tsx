0200331d1f82b631d637dd4950289f6fa6cbe410
betajohn <betajohn40@gmail.com>
2024-05-02 04:43:02 -0400

    Update managed to make carousel work as intended

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
-import CarouselOld from '@/components/admin/products/productForm/FormPCarousel';
+'use client';
 
+import CarouselOld from '@/components/admin/products/productForm/FormPCarousel';
+import { useForm } from 'react-hook-form';
+import { ProductFormSchemaType } from '@/lib/zodSchemas';
 export default function Page() {
+  const form = useForm<ProductFormSchemaType>({
+    defaultValues: {
+      name: '',
+      description: '',
+      price: -1,
+      active: true,
+      category: {
+        name: '',
+        categ_id: '',
+      },
+      images: [],
+    },
+  });
+
   return (
-    <div>
-      <CarouselOld />
+    <div className="w-[600px]">
+      <CarouselOld form={form} />
     </div>
   );
 }
--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -2,15 +2,51 @@
 import { Button } from '@/components/ui/button';
 import { SelectItemIndicator } from '@radix-ui/react-select';
 import useEmblaCarousel from 'embla-carousel-react';
-import { useState } from 'react';
+import { cn } from '@/lib/utils';
+import { useState, useEffect, useCallback } from 'react';
+import {
+  Plus,
+  Trash2Icon,
+  CornerUpLeft,
+  CornerUpRight,
+  ImageUp,
+  X,
+} from 'lucide-react';
 
 export default function Page() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({
+  const [emblaRef, emblaMainApi] = useEmblaCarousel({
     loop: false,
+    watchSlides: true,
+  });
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: 'keepSnaps',
+    dragFree: true,
     watchSlides: true,
   });
   const [slides, setSlides] = useState(['q']);
   const [selected, setSelected] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaMainApi || !emblaThumbsApi) return;
+      emblaMainApi.scrollTo(index);
+    },
+    [emblaMainApi, emblaThumbsApi]
+  );
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return;
+    setSelectedIndex(emblaMainApi.selectedScrollSnap());
+    emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap());
+  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!emblaMainApi) return;
+    onSelect();
+    emblaMainApi.on('select', onSelect);
+    emblaMainApi.on('reInit', onSelect);
+  }, [emblaMainApi, onSelect]);
 
   return (
     <div>
@@ -41,17 +77,18 @@ export default function Page() {
 
       <Button
         onClick={() => {
-          let i = emblaApi?.selectedScrollSnap();
+          let i = emblaMainApi?.selectedScrollSnap();
           if (i === 0) {
             console.log("can't move left");
             return;
           }
-          console.log(emblaApi?.selectedScrollSnap());
+          console.log(emblaMainApi?.selectedScrollSnap());
           const newImages = [...slides];
           newImages[i - 1] = slides[i];
           newImages[i] = slides[i - 1];
           setSlides(newImages);
-          emblaApi?.scrollTo(i - 1);
+          emblaMainApi?.scrollTo(i - 1);
+          onThumbClick(i - 1);
           setSelected(i - 1);
           console.log('moved left');
         }}
@@ -61,22 +98,39 @@ export default function Page() {
 
       <Button
         onClick={() => {
-          let i = emblaApi?.selectedScrollSnap();
+          let i = emblaMainApi?.selectedScrollSnap();
           if (i === slides.length - 1) {
             console.log("can't move right");
             return;
           }
-          console.log(emblaApi?.selectedScrollSnap());
+
           const newImages = [...slides];
           newImages[i + 1] = slides[i];
           newImages[i] = slides[i + 1];
           setSlides(newImages);
-          emblaApi?.scrollTo(i + 1);
+          emblaMainApi?.scrollTo(i + 1);
+          onThumbClick(i + 1);
           setSelected(i + 1);
           console.log('moved right');
         }}
       >
         move right
+      </Button>
+      <Button
+        onClick={() => {
+          let i = emblaMainApi?.selectedScrollSnap();
+          const newImages: string[] = [];
+          for (let x = 0; x < slides.length; x++) {
+            if (x !== i) {
+              newImages.push(slides[x]);
+            }
+          }
+
+          setSlides(newImages);
+          console.log('deleted ' + i);
+        }}
+      >
+        delete
       </Button>
 
       <Button
@@ -86,7 +140,34 @@ export default function Page() {
       >
         print
       </Button>
-      {`${emblaApi?.selectedScrollSnap() + 1}/${slides.length}`}
+      {`${emblaMainApi?.selectedScrollSnap() + 1}/${slides.length}`}
+
+      <div
+        className="embla-thumbs__viewport overflow-hidden"
+        ref={emblaThumbsRef}
+      >
+        <div className="embla-thumbs__container flex">
+          {slides.map((x, index) => (
+            <div
+              key={index}
+              className={cn(
+                'embla-thumbs__slide rounded-lg border bg-card text-card-foreground shadow-sm flex-[0_0_22%] min-w-0 ml-3 sm:flex-[0_0_15%] border-foreground',
+                index === selectedIndex && 'border-primary text-primary'
+              )}
+            >
+              <button
+                onClick={() => {
+                  onThumbClick(index);
+                }}
+                type="button"
+                className="w-full h-full"
+              >
+                {x === 'add' ? <Plus className="m-auto h-4" /> : index + 1}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
